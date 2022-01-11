75c4ee4e031b56eacf82277579697748a2265cb6
thaonhunguyen <nguyenthaonhu2610@gmail.com>
2022-01-11 12:58:59 +0000

    adding metadata path

--- configs.py
+++ configs.py
@@ -1,4 +1,5 @@
 import os
+import os.path as osp
 from glob import glob
 from tqdm import tqdm
 
@@ -8,12 +9,12 @@ dataset_name = 'V3C'
 if dataset_name == 'LSC':
     DATASET_NAME = dataset_name
     MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
-    IMAGE_NAME_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_image_names.joblib')
+    IMAGE_NAME_PATH = osp.join(MASTER_PATH, 'results', f'{DATASET_NAME}_image_names.joblib')
 
     DATASET_PATH = '/mnt/data/lsc2020' 
     error_image_file = '/home/ntnhu/projects/LSC2021/LSC_clip_model/error_images.joblib'
-    FEATURE_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_features')
-    if not os.path.isdir(FEATURE_PATH):
+    FEATURE_PATH = osp.join(MASTER_PATH, 'results', f'{DATASET_NAME}_features')
+    if not osp.isdir(FEATURE_PATH):
         os.mkdir(FEATURE_PATH)
 
 elif dataset_name == 'V3C1':
@@ -21,11 +22,11 @@ elif dataset_name == 'V3C1':
     MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
     # DATASET_MASTER_PATH = '/home/SharedFolder/VBS2021/dataset'
     DATASET_MASTER_PATH = '/mnt/SEAGATE/root/V3C1'
-    IMAGE_NAME_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_image_names.joblib')
+    IMAGE_NAME_PATH = osp.join(MASTER_PATH, 'results', f'{DATASET_NAME}_image_names.joblib')
 
-    DATASET_PATH = os.path.join(DATASET_MASTER_PATH, 'resized_keyframes')
-    FEATURE_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_features')
-    if not os.path.isdir(FEATURE_PATH):
+    DATASET_PATH = osp.join(DATASET_MASTER_PATH, 'resized_keyframes')
+    FEATURE_PATH = osp.join(MASTER_PATH, 'results', f'{DATASET_NAME}_features')
+    if not osp.isdir(FEATURE_PATH):
         os.mkdir(FEATURE_PATH)
         
 elif dataset_name == 'V3C':
@@ -33,9 +34,10 @@ elif dataset_name == 'V3C':
     MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
     # DATASET_MASTER_PATH = '/home/SharedFolder/VBS2021/dataset'
     DATASET_MASTER_PATH = '/mnt/DEAKIN/VBS2022'
-    IMAGE_NAME_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_image_names.joblib')
+    METADATA_PATH = osp.join(DATASET_MASTER_PATH, 'metadata')
+    IMAGE_NAME_PATH = osp.join(METADATA_PATH, f'image_names.joblib')
 
-    DATASET_PATH = os.path.join(DATASET_MASTER_PATH, 'keyframes')
-    FEATURE_PATH = os.path.join(MASTER_PATH, 'results', f'{DATASET_NAME}_features')
-    if not os.path.isdir(FEATURE_PATH):
+    DATASET_PATH = osp.join(DATASET_MASTER_PATH, 'keyframes')
+    FEATURE_PATH = osp.join(METADATA_PATH, f'{DATASET_NAME}_features')
+    if not osp.isdir(FEATURE_PATH):
         os.mkdir(FEATURE_PATH)
