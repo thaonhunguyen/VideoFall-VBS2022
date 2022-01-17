c14405239bc061f649af1950803097c564d6325d
thaonhunguyen <nguyenthaonhu2610@gmail.com>
2022-01-17 13:38:24 +0000

    update

--- encode_dataset.py
+++ encode_dataset.py
@@ -9,10 +9,6 @@ parser.add_argument('--query', '-q', type=str, default='wedding', help='Input qu
 parser.add_argument('--generate_features', '-g', default=True, help='Whether you wanna generate features or not')
 
 def main(args):
-    # data = dataset(src_path=DATASET_PATH, feature_path=FEATURE_PATH)
-    # data.get_file_name()
-    # data.preprocess_dataset(entire_dataset=False)
-    # print("Features: ", data.features)
     print("Dataset name: ", DATASET_NAME)
     clip = CLIPSearchEngine(src_path=DATASET_PATH, feature_path=FEATURE_PATH, generate_features=args.generate_features)
     print(args.generate_features)
