379ccedf4281620a5029ad7c66ed4b888c902861
thaonhunguyen <nguyenthaonhu2610@gmail.com>
2022-06-15 08:16:20 +0100

    change directory in configs

--- configs.py
+++ configs.py
@@ -6,14 +6,28 @@ from tqdm import tqdm
 model_name = 'L14'
 # model_name = 'L14_336'
 # model_name = 'B32'
+dataset_name = 'LSC22'
 
-DATASET_NAME = 'V3C'
-MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
-# DATASET_MASTER_PATH = '/home/SharedFolder/VBS2021/dataset'
-DATASET_MASTER_PATH = '/mnt/deakin/VBS2022'
-METADATA_PATH = osp.join(DATASET_MASTER_PATH, 'metadata')
-EMBEDDING_PATH = osp.join(DATASET_MASTER_PATH, 'embedding_features')
-IMAGE_NAME_PATH = osp.join(METADATA_PATH, f'image_names.joblib')
+if dataset_name == 'V3C':
+    DATASET_NAME = dataset_name
+    MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
+    # DATASET_MASTER_PATH = '/home/SharedFolder/VBS2021/dataset'
+    DATASET_MASTER_PATH = '/mnt/deakin/VBS2022'
+    METADATA_PATH = osp.join(DATASET_MASTER_PATH, 'metadata')
+    EMBEDDING_PATH = osp.join(DATASET_MASTER_PATH, 'embedding_features')
+    IMAGE_NAME_PATH = osp.join(METADATA_PATH, f'image_names.joblib')
+elif dataset_name == 'LSC22':
+    DATASET_NAME = dataset_name
+    MASTER_PATH = '/home/ntnhu/projects/VideoFall-VBS2022'
+    # DATASET_MASTER_PATH = '/home/SharedFolder/VBS2021/dataset'
+    DATASET_MASTER_PATH = '/mnt/data/lsc2022'
+    METADATA_PATH = osp.join('/mnt/deakin/lsc2022', 'metadata')
+    EMBEDDING_PATH = osp.join('/mnt/deakin/lsc2022', 'embedding_features')
+    IMAGE_NAME_PATH = osp.join('/mnt/deakin/lsc2022', 'image_names.joblib')
+    
+# METADATA_PATH = osp.join(DATASET_MASTER_PATH, 'metadata')
+# EMBEDDING_PATH = osp.join(DATASET_MASTER_PATH, 'embedding_features')
+# IMAGE_NAME_PATH = osp.join(METADATA_PATH, f'image_names.joblib')
 
 KEYFRAME_PATH = osp.join(DATASET_MASTER_PATH, 'keyframes')
 if model_name == 'B32':
